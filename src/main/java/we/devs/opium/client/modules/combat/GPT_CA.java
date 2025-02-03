575845d6be46ba3248391e3661bcfce5d5c2d488
AnonCx <callmemika1805@gmail.com>
2025-02-04 00:20:15 +0100

    Absolut Cinema

---
+++ src/main/java/we/devs/opium/client/modules/combat/GPT_CA.java
@@ -0,0 +1,23 @@
+package we.devs.opium.client.modules.combat;
+
+import we.devs.opium.api.manager.module.Module;
+import we.devs.opium.api.manager.module.RegisterModule;
+
+@RegisterModule(name = "GPT-CA", description = "GPT-CA", tag = "GPT-CA", category = Module.Category.COMBAT)
+public class GPT_CA extends Module {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+    @Override
+    public void onTick() {
+        super.onTick();
+    }
+}
