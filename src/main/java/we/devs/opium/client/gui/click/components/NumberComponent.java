41b0caee94eb7687d5761087c32dac3bb8dc9e48
AnonCx <callmemika1805@gmail.com>
2025-02-05 00:49:38 +0100

    ahahahah

--- src/main/java/we/devs/opium/client/gui/click/components/NumberComponent.java
+++ src/main/java/we/devs/opium/client/gui/click/components/NumberComponent.java
@@ -1,29 +1,18 @@
 package we.devs.opium.client.gui.click.components;
 
-import com.sun.jna.platform.KeyboardUtils;
 import we.devs.opium.Opium;
 import we.devs.opium.api.utilities.MathUtils;
 import we.devs.opium.api.utilities.RenderUtils;
-import we.devs.opium.api.utilities.TimerUtils;
 import we.devs.opium.client.gui.click.manage.Component;
 import we.devs.opium.client.gui.click.manage.Frame;
 import we.devs.opium.client.values.impl.ValueNumber;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.util.Formatting;
 
-import java.awt.*;
-import java.awt.datatransfer.DataFlavor;
-
 public class NumberComponent extends Component {
     private final ValueNumber value;
     private float sliderWidth;
     private boolean dragging;
-    private boolean listening;
-    private String currentString = "";
-    private final TimerUtils timer = new TimerUtils();
-    private final TimerUtils backTimer = new TimerUtils();
-    private boolean selecting = false;
-    private boolean line = false;
 
     public NumberComponent(ValueNumber value, int offset, Frame parent) {
         super(offset, parent);
@@ -32,19 +21,11 @@ public class NumberComponent extends Component {
 
     @Override
     public void render(DrawContext context, int mouseX, int mouseY, float partialTicks) {
-        if (this.timer.hasTimeElapsed(400L)) {
-            this.line = !this.line;
-            this.timer.reset();
-        }
-        RenderUtils.drawRect(context.getMatrices(),this.getX() + 1, this.getY(), (float)(this.getX() + 1) + this.sliderWidth, this.getY() + 14, Opium.CLICK_GUI.getColor());
-        if (this.selecting) {
-            RenderUtils.drawRect(context.getMatrices(),(float)(this.getX() + 3) + mc.textRenderer.getWidth(this.value.getTag() + " "), this.getY() + 3, (float)(this.getX() + 3) + mc.textRenderer.getWidth(this.value.getTag() + " ") + mc.textRenderer.getWidth(this.currentString), (float)this.getY() + mc.textRenderer.fontHeight + 3.0f, new Color(Color.LIGHT_GRAY.getRed(), Color.LIGHT_GRAY.getGreen(), Color.LIGHT_GRAY.getBlue(), 100));
-        }
-        if (this.listening) {
-            RenderUtils.drawString(context.getMatrices(), this.value.getTag() + " " + Formatting.GRAY + this.currentString + (this.selecting ? "" : (this.line ? "|" : "")), this.getX() + 3, this.getY() + 3, -1);
-        } else {
-            RenderUtils.drawString(context.getMatrices(), this.value.getTag() + " " + Formatting.GRAY + this.value.getValue() + (this.value.getType() == 1 ? ".0" : ""), this.getX() + 3, this.getY() + 3, -1);
-        }
+        // Draw the slider background
+        RenderUtils.drawRect(context.getMatrices(), this.getX() + 1, this.getY(), (float)(this.getX() + 1) + this.sliderWidth, this.getY() + 14, Opium.CLICK_GUI.getColor());
+
+        // Draw the value text
+        RenderUtils.drawString(context.getMatrices(), this.value.getTag() + " " + Formatting.GRAY + this.value.getValue() + (this.value.getType() == 1 ? ".0" : ""), this.getX() + 3, this.getY() + 3, -1);
     }
 
     @Override
@@ -53,6 +34,7 @@ public class NumberComponent extends Component {
         if (this.value.getParent() != null) {
             this.setVisible(this.value.getParent().isOpen());
         }
+
         double difference = Math.min(98, Math.max(0, mouseX - this.getX()));
         if (this.value.getType() == 1) {
             this.sliderWidth = 98.0f * (float)(this.value.getValue().intValue() - this.value.getMinimum().intValue()) / (float)(this.value.getMaximum().intValue() - this.value.getMinimum().intValue());
@@ -91,9 +73,6 @@ public class NumberComponent extends Component {
     public void mouseClicked(int mouseX, int mouseY, int mouseButton) {
         if (this.isHovering(mouseX, mouseY) && mouseButton == 0) {
             this.dragging = true;
-        } else if (mouseButton == 1 && this.isHovering(mouseX, mouseY)) {
-            this.listening = !this.listening;
-            this.currentString = this.value.getValue().toString();
         }
     }
 
@@ -101,88 +80,6 @@ public class NumberComponent extends Component {
     public void mouseReleased(int mouseX, int mouseY, int state) {
         super.mouseReleased(mouseX, mouseY, state);
         this.dragging = false;
-    }
-
-    @Override
-    public void charTyped(char typedChar, int keyCode) {
-        super.charTyped(typedChar, keyCode);
-        this.backTimer.reset();
-        if (this.listening) {
-            if (keyCode == 1) {
-                this.selecting = false;
-                return;
-            }
-            if (keyCode == 28) {
-                this.updateString();
-                this.selecting = false;
-                this.listening = false;
-            } else if (keyCode == 14) {
-                this.currentString = this.selecting ? "" : this.removeLastCharacter(this.currentString);
-                this.selecting = false;
-            } else if (keyCode == 47 && (KeyboardUtils.isPressed(157) || KeyboardUtils.isPressed(29))) {
-                try {
-                    this.currentString = this.currentString + Toolkit.getDefaultToolkit().getSystemClipboard().getData(DataFlavor.stringFlavor);
-                }
-                catch (Exception exception) {
-                    exception.printStackTrace();
-                }
-            } else if (isValidChatCharacter(typedChar)) {
-                this.currentString = this.selecting ? "" + typedChar : this.currentString + typedChar;
-                this.selecting = false;
-            }
-            if (keyCode == 30 && KeyboardUtils.isPressed(29)) {
-                this.selecting = true;
-            }
-        }
-    }
-
-    private void updateString() {
-        block1:
-        if (this.currentString.length() > 0) {
-            if (this.value.getType() == 1) {
-                try {
-                    if (Integer.parseInt(this.currentString) <= this.value.getMaximum().intValue() && Integer.parseInt(this.currentString) >= this.value.getMinimum().intValue()) {
-                        this.value.setValue(Integer.parseInt(this.currentString));
-                        break block1;
-                    }
-                    this.value.setValue(this.value.getValue());
-                } catch (NumberFormatException e) {
-                    this.value.setValue(this.value.getValue());
-                }
-            } else if (this.value.getType() == 3) {
-                try {
-                    if (!(Float.parseFloat(this.currentString) > this.value.getMaximum().floatValue()) && !(Float.parseFloat(this.currentString) < this.value.getMinimum().floatValue())) {
-                        this.value.setValue(Float.parseFloat(this.currentString));
-                        break block1;
-                    }
-                    this.value.setValue(this.value.getValue());
-                } catch (NumberFormatException e) {
-                    this.value.setValue(this.value.getValue());
-                }
-            } else if (this.value.getType() == 2) {
-                try {
-                    if (!(Double.parseDouble(this.currentString) > this.value.getMaximum().doubleValue()) && !(Double.parseDouble(this.currentString) < this.value.getMinimum().doubleValue())) {
-                        this.value.setValue(Double.parseDouble(this.currentString));
-                    } else {
-                        this.value.setValue(this.value.getValue());
-                    }
-                } catch (NumberFormatException e) {
-                    this.value.setValue(this.value.getValue());
-                }
-            }
-        }
-        this.currentString = "";
-    }
-
-    private boolean isValidChatCharacter(char c) {
-        return c >= ' ' && c != 127;
-    }
-
-    private String removeLastCharacter(String input) {
-        if (input.length() > 0) {
-            return input.substring(0, input.length() - 1);
-        }
-        return input;
     }
 
     public ValueNumber getValue() {
