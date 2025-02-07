88f6269b5c6bfe571cc525473a96a8768d810e8d
anoncx <callmemika1805@gmail.com>
2025-02-07 16:21:05 +0100

    added animated Capes

--- src/main/java/we/devs/opium/api/manager/CapeManager.java
+++ src/main/java/we/devs/opium/api/manager/CapeManager.java
@@ -1,15 +1,58 @@
 package we.devs.opium.api.manager;
 
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.resource.ResourceManager;
 import net.minecraft.util.Identifier;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CapeManager {
-    private static Identifier currentCape = Identifier.of("opium", "textures/capes/cape.png");
+    private static Identifier currentCape = Identifier.of("opium", "textures/capes/opium.png");
+    private static List<Identifier> animatedCapeFrames = new ArrayList<>();
+    private static int currentFrame = 0;
+    private static long lastFrameTime = 0;
+    private static final long FRAME_DELAY = 100; // Delay between frames in milliseconds
 
     public static Identifier getCape() {
+        if (!animatedCapeFrames.isEmpty()) {
+            long currentTime = System.currentTimeMillis();
+            if (currentTime - lastFrameTime >= FRAME_DELAY) {
+                currentFrame = (currentFrame + 1) % animatedCapeFrames.size();
+                lastFrameTime = currentTime;
+            }
+            return animatedCapeFrames.get(currentFrame);
+        }
         return currentCape;
     }
 
     public static void setCurrentCape(Identifier cape) {
         currentCape = cape;
+        animatedCapeFrames.clear(); // Stop animation when switching to a static cape
     }
+
+    public static void setAnimatedCape(String animationName) {
+        animatedCapeFrames = loadAnimationFrames(animationName);
+        currentFrame = 0;
+        lastFrameTime = System.currentTimeMillis();
+    }
+
+    public static List<Identifier> loadAnimationFrames(String animationName) {
+        List<Identifier> frames = new ArrayList<>();
+        ResourceManager resourceManager = MinecraftClient.getInstance().getResourceManager();
+
+        // Look for frames in the folder
+        int frameCount = 1;
+        while (true) {
+            Identifier frameLocation = Identifier.of("opium", "textures/capes/animated_cape/" + animationName + "/frame_" + frameCount + ".png");
+            if (resourceManager.getResource(frameLocation).isEmpty()) {
+                break; // Stop if no more frames are found
+            }
+            frames.add(frameLocation);
+            frameCount++;
+        }
+
+        return frames;
+    }
+
 }
--- src/main/java/we/devs/opium/client/commands/CommandCape.java
+++ src/main/java/we/devs/opium/client/commands/CommandCape.java
@@ -6,13 +6,21 @@ import we.devs.opium.api.manager.command.Command;
 import we.devs.opium.api.manager.command.RegisterCommand;
 import we.devs.opium.api.utilities.ChatUtils;
 
-@RegisterCommand(name="Cape", description="Let's you change your cape", syntax="cape <name>", aliases={"cape"})
+import java.util.List;
+
+@RegisterCommand(name="Cape", description="Let's you change your cape", syntax="cape static/animated <name>", aliases={"cape"})
 public class CommandCape extends Command {
     @Override
     public void onCommand(String[] args) {
-        if (args.length == 1) {
-            setCape(args[0]);
-            ChatUtils.sendMessage("Cape set to " + args[0],"Cape");
+        if (args.length == 2) {
+            if (args[0].equals("static")) {
+                setCape(args[1].toLowerCase());
+            } else if (args[0].equals("animated")) {
+                setAnimatedCape(args[1].toLowerCase());
+            } else {
+                this.sendSyntax();
+            }
+            ChatUtils.sendMessage("Cape set to " + args[1],"Cape");
         } else {
             this.sendSyntax();
         }
@@ -21,5 +29,13 @@ public class CommandCape extends Command {
     private static void setCape(String capeName) {
         Identifier newCape = Identifier.of("opium", "textures/capes/" + capeName + ".png");
         CapeManager.setCurrentCape(newCape);
+    }
+    private static void setAnimatedCape(String animationName) {
+        List<Identifier> frames = CapeManager.loadAnimationFrames(animationName);
+        if (frames.isEmpty()) {
+            ChatUtils.sendMessage("No animated cape found for animation " + animationName, "Cape");
+            return;
+        }
+        CapeManager.setAnimatedCape(animationName);
     }
 }
